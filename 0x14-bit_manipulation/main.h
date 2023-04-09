662017d494c07ac3e90c348fe356992671b4b15d
fifithatlady <felistasfenyere2224@gmail.com>
2023-04-09 09:29:59 -0700

    headerfile modified

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-#include <stdio.h>
-#include <unistd.h>
-#include  _putchar
+
 unsigned int binary_to_uint(const char *b)
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
